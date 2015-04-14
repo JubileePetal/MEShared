0ed8d0da010bbb6ba0625a19b59c807d2de40c37
Circus <circus@lindseytop.(none)>
2015-04-14 14:32:07 +0200

    added currency, instrument and some attributes to Order

--- src/Order.java
+++ src/Order.java
@@ -2,9 +2,13 @@
 public class Order {
 	
 	String message;
+	
+	private Currency currency;
+	private Instrument instrument;
 
 	public Order() {
 		// TODO Auto-generated constructor stub
+		message = "Hej";
 	}
 
 }
