0d23b3bc88d02e6ead401f0fb27f1e3c6d68a2f7
Circus <circus@lindseytop.(none)>
2015-04-17 14:28:37 +0200

    updated order class with more attributes and setters

--- src/models/Order.java
+++ src/models/Order.java
@@ -2,27 +2,45 @@ package models;
 
 public class Order {
 	
-	private String message;
+	int currency;
+	int instrument;	
+	int buyOrSell;
 	
-	Currency currency;
-	Instrument instrument;
-	
-	String ownerNick;
+	String myOwner;
 	int quantity;
-	boolean isActivated;
+	boolean isActive;
 	
 	long timeEnteredSystem;
 	long timeEnteredOrderBook;
 	
 	public Order(long timeEnteredSystem) {
 		
+		isActive = false;
 		this.timeEnteredSystem = timeEnteredSystem;
 		
 	}
 	
-	public String getMessage() {
-		
-		return message;
+	public void setOrderType(int currency, int instrument, int buyOrSell) {
+		this.currency = currency;
+		this.instrument = instrument;
+		this.buyOrSell = buyOrSell;
 	}
+	
+	public void setOrderOwner(String ownerNick) {
+		myOwner = ownerNick;
+	}
+	
+	public void setOrderQuantity(int orderQuantity) {
+		this.quantity = orderQuantity;
+	}
+	
+	public void activateOrder() {
+		isActive = true;
+	}
+	
+	public void setTimeEnteredOrderBook(long time) {
+		timeEnteredOrderBook = time;
+	}
+
 	
 }
