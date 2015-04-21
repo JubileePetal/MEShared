53451e0b3fa2c15f40080f6e58950c3f3eff5533
tinyeyra <anna@Annas-MacBook-Pro-2.local>
2015-04-21 09:29:28 +0200

    Added the Message class

---
+++ src/models/Message.java
@@ -0,0 +1,36 @@
+package models;
+
+public class Message {
+
+	private int type;
+	private String message;
+
+
+	public Message() {
+	
+	}
+
+
+
+	public int getType() {
+		return type;
+	}
+	
+
+
+
+	public String getMessage() {
+		return message;
+	}
+	
+
+
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	
+	
+	
+}
