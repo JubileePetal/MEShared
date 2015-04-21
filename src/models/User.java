d77849a65e25cdd67e5444af2bd4be1048b6856b
tinyeyra <anna@Annas-MacBook-Pro-2.local>
2015-04-21 09:37:54 +0200

    Added the user class

---
+++ src/models/User.java
@@ -0,0 +1,22 @@
+package models;
+
+public class User {
+
+	private String username;
+	private int userType;
+	
+	public User() {
+		
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public int getUserType() {
+		return userType;
+	}
+	
+	
+
+}
